fdff286b4c8c068f0f60c6c767daf98e90cc4bad
Bollesyy <33631907+Bollesyy@users.noreply.github.com>
2019-10-21 18:24:04 -0400

    first commit
    first commit

---
+++ blahblah/Assets/Scripts/Glasses.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glasses : MonoBehaviour
+{
+
+   public static bool areGlassesEquiped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        makeGlassesFalse();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !areGlassesEquiped)
+        {
+            EquipGlasses();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Q) && areGlassesEquiped)
+        {
+            TakeOffGlasses();
+        }
+    }
+
+    void EquipGlasses()
+    {
+        Debug.Log("Equipped glases");
+        makeGlassesTrue();
+    }
+
+    void TakeOffGlasses()
+    {
+        Debug.Log("Took off glases");
+        makeGlassesFalse();
+    }
+
+    void makeGlassesFalse()
+    {
+        areGlassesEquiped = false;
+    }
+
+    void makeGlassesTrue()
+    {
+        areGlassesEquiped = true;
+    }
+}
